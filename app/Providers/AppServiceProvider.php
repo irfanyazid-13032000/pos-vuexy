c0c804c0d03eab3f2e2d05acb8cb958cbd46316a
irfanyazid-13032000 <irfanyazid18@gmail.com>
2023-08-03 18:00:37 +0700

    berhasil integrasi template vuexy (hanya menu dashboard)

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\View;
+use DB;
+use Illuminate\Support\Facades\URL;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    public function boot()
+    {
+        Schema::defaultStringLength(191);
+        
+         App::setLocale('id');
+    }
+}
